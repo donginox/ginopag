761be3df585eeb44711c4f8d13009afba0dd35d5
donginox <gimontehermoso616@gmail.com>
2025-08-12 16:32:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -889,8 +889,8 @@ document.head.appendChild(breakingStyles);
 
 // Crear el cliente de Supabase
 const supabase = Supabase.createClient(
-  'https://<https://fsmpgzystzedgwrinukb.supabase.co>.supabase.co',  // Reemplaza con tu URL de Supabase
-  '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzbXBnenlzdHplZGd3cmludWtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5MTk2ODksImV4cCI6MjA3MDQ5NTY4OX0.6bXgSLXvqKRt7T5yt-togXQQX4fbW-zk0z-4PRHt2yg>'  // Reemplaza con tu clave de API
+  'https://fsmpgzystzedgwrinukb.supabase.co',  // URL de Supabase
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzbXBnenlzdHplZGd3cmludWtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5MTk2ODksImV4cCI6MjA3MDQ5NTY4OX0.6bXgSLXvqKRt7T5yt-togXQQX4fbW-zk0z-4PRHt2yg'  // Clave de API (anon key)
 );
 
 // Ejemplo de consulta a la base de datos
