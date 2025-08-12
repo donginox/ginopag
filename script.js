5952a60fd03c2641e1c707535bbf166738defe92
donginox <gimontehermoso616@gmail.com>
2025-08-12 16:36:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -885,12 +885,10 @@ breakingStyles.textContent = `
     }
 `;
 document.head.appendChild(breakingStyles);
-// script.js
-
 // Crear el cliente de Supabase
-const supabase = Supabase.createClient(
+const supabase = supabase.createClient(
   'https://fsmpgzystzedgwrinukb.supabase.co',  // URL de Supabase
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzbXBnenlzdHplZGd3cmludWtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5MTk2ODksImV4cCI6MjA3MDQ5NTY4OX0.6bXgSLXvqKRt7T5yt-togXQQX4fbW-zk0z-4PRHt2yg'  // Clave de API (anon key)
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzbXBnenlzdHplZGd3cmludWtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5MTk2ODksImV4cCI6MjA3MDQ5NTY4OX0.6bXgSLXvqKRt7T5yt-togXQQX4fbW-zk0z-4PRHt2yg'  // Reemplaza con tu clave de API
 );
 
 // Ejemplo de consulta a la base de datos
